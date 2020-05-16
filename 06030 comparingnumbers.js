afdc8b3e72a4b555d63d1516d4c2e47a56415667
John Purcell <john.purcell@caveofprogramming.com>
2020-05-16 15:26:41 +0200

    Created

---
+++ 06030 comparingnumbers.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let daysInWeek = 7;
+
+console.log("daysInWeek == 6", daysInWeek == 6);    // Equality test
+console.log("daysInWeek === 6", daysInWeek === 6);  // Identity test
+console.log("daysInWeek != 6", daysInWeek != 6);    // Not equal
+console.log("daysInWeek !=== 6", daysInWeek !== 6); // Not identical
+console.log("daysInWeek < 8", daysInWeek < 8);  // Less than
+console.log("daysInWeek > 8", daysInWeek > 8);  // Greater than
+console.log("daysInWeek <= 7", daysInWeek <= 7);    // Less than or equal to
+console.log("daysInWeek >= 8", daysInWeek >= 8);    // Greater than or equal to
