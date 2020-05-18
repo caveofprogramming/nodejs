c1e5c69421929d2058cf46430dcf83db31f47fbc
John Purcell <john.purcell@caveofprogramming.com>
2020-05-18 13:36:02 +0200

    Created

---
+++ 01110 comparingstrings.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let text = 'cat';
+
+console.log("text == 'cat'", text == 'cat');
+console.log("text === 'cat'", text === 'cat');
+
+console.log("text != 'cat'", text != 'cat');
+console.log("text !== 'cat'", text !== 'cat');
+
+console.log("text < 'dog'", text < 'dog');
+console.log("text >= 'dog'", text >= 'dog');
+
+console.log("'Dog' >= 'dog'", 'Dog' >= 'dog');
